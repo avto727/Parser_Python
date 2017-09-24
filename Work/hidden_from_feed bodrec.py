d87b4745ef16a7ee8cbd8f1cc67425f68a265ba7
avto727 <avto727@bk.ru>
2017-09-24 10:52:22 +0300

    del

--- Work/hidden_from_feed bodrec.py
+++ Work/hidden_from_feed bodrec.py
@@ -7,7 +7,7 @@
 import vk
 import time
 
-session = vk.Session(access_token='4486fe808ab2a819e8b1596c4f9a9bb120c7c8ddc4c1ec79d5dc5d18100f7f93e52007853f15ff10da1d1')
+session = vk.Session(access_token='4486fe808ab...15ff10da1d1')
 vkapi = vk.API(session)
 friends = vkapi('friends.get')
 i = 0 #Здесь и
